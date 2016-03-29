cdb9b172bd7982db99b579e4b76758ebaee52d8e
BAN Jun <banjun@gmail.com>
2016-03-29 19:44:39 +0900

    add :eyes: for build phase name

--- bansan-setup.rb
+++ bansan-setup.rb
@@ -3,12 +3,12 @@ require 'xcodeproj'
 f = Dir.glob("*.xcodeproj").first
 p = Xcodeproj::Project.open f
 t = p.targets.first
-if t.build_phases.any?{|p| p.respond_to?(:name) && p.name == "bansan check"} then
+if t.build_phases.any?{|p| p.respond_to?(:name) && p.name.include?("bansan check")} then
   puts "bansan check already installed to #{t.name}."
   exit 0
 end
 puts "add bansan check build phase to #{t.name}..."
-t.new_shell_script_build_phase("bansan check").tap{|b|
+t.new_shell_script_build_phase("👀 bansan check").tap{|b|
   b.shell_path = '/bin/zsh'
   b.show_env_vars_in_log = '0'
   b.shell_script = <<-EOS
