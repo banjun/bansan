73726f2d7b40802b9fbca37f9aee6a83ef4a5ff6
BAN Jun <banjun@gmail.com>
2016-04-01 15:31:53 +0900

    add check for deinit initialized lazy var referencing to self (some case not implemented)

--- bansan.swift
+++ bansan.swift
@@ -23,8 +23,10 @@ func traverse(substructures: [SourceKitRepresentable],
 
 func check(file: File) {
     let s = Structure(file: file)
-    let substructures = s.dictionary["key.substructure"] as? [SourceKitRepresentable]
-    traverse(substructures ?? []) { structure, substructures in
+    let substructures = s.dictionary["key.substructure"] as? [SourceKitRepresentable] ?? []
+
+    // check super call requirements
+    traverse(substructures) { structure, substructures in
         guard let kind = structure["key.kind"] as? String,
             let name = structure["key.name"] as? String,
             let attrs = structure["key.attributes"] as? [SourceKitRepresentable] else {
@@ -58,6 +60,42 @@ func check(file: File) {
                     let line = file.contents.lineAndCharacterForByteOffset(byteOffset)?.line ?? 1
                     print("\(file.path!):\(line): warning: \(name) requires super call")
                 }
+        }
+    }
+
+    // check deinit initialized lazy var referencing self
+    var lazyVarsRefsSelf = [String]()
+    traverse(substructures) { structure, substructures in
+        guard let kind = structure["key.kind"] as? String,
+            let name = structure["key.name"] as? String,
+            let attrs = structure["key.attributes"] as? [SourceKitRepresentable] else {
+                return
+        }
+        let attributes = attrs.flatMap({(($0 as? [String: SourceKitRepresentable]) ?? [:]).values})
+
+        if attributes.contains({($0 as? String) == "source.decl.attribute.lazy"}) &&
+            kind == "source.lang.swift.decl.var.instance" {
+                // TODO: match only if its definition expr contains self
+                lazyVarsRefsSelf.append(name)
+        }
+    }
+    traverse(substructures) { structure, substructures in
+        guard let kind = structure["key.kind"] as? String,
+            let name = structure["key.name"] as? String else {
+                return
+        }
+        guard name == "deinit" &&
+            kind == "source.lang.swift.decl.function.method.instance" else {
+                return
+        }
+        traverse(substructures ?? []) { descendant, substructures in
+            // TODO: matching to identifiers
+            guard let descendantName = descendant["key.name"] as? String else { return }
+            if lazyVarsRefsSelf.contains({descendantName.containsString($0)}) {
+                let descendantOffset = Int(descendant["key.offset"] as? Int64 ?? 0)
+                let line = file.contents.lineAndCharacterForByteOffset(descendantOffset)?.line ?? 1
+                print("\(file.path!):\(line): warning: lazy var \(descendantName) referencing self cannot be initialized in deinit")
+            }
         }
     }
 }
