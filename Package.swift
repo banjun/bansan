91b661999c1b1a361c52e4afeb1d30ae5393b869
BAN Jun <banjun@gmail.com>
2017-11-09 15:12:45 +0900

    migrate to Swift Package Manager 4 from Carthage

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "bansan",
+    products: [
+        .executable(name: "bansan", targets: ["bansan"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/jpsim/SourceKitten", from: "0.18.2"),
+    ],
+    targets: [
+        .target(
+            name: "bansan",
+            dependencies: ["SourceKittenFramework"],
+            path: ".",
+            sources: ["main.swift"]),
+    ]
+)
